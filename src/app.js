6133781930c86124083228bf9916bea0ea447520
Lukas Koehler <koehler.lukas997@gmail.com>
2019-09-13 13:46:57 +0200

    Added package.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const path    = require('path');
 const hbs     = require('hbs');
+
+const path    = require('path');
 
 const geocode  = require('./utils/geocode');
 const forecast = require('./utils/forecast');
